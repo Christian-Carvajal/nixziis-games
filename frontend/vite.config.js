4e1aa4c4b6dc8e61317be589bffa6a7fc8d24104
CHRISTIAN EZEKIEL L. CARVAJAL <christianezekielcarvajalgithub@gmail.com>
2026-04-05 15:45:55 +0800

    Set base path for Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/nixziis-games/",   // 👈 THIS IS THE FIX
   server: {
     port: 5173
   },
